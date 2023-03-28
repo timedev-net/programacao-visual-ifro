b1c2f5e7cf116a6c1d03c3de4f99adfc193cb5f9
timedev-net <drfrota.adv@gmail.com>
2023-03-27 22:03:38 -0400

    fazendo a parte de salvar o arquivo

---
+++ GeracaoArquivoTexto/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace GeracaoArquivoTexto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nr. de Funcionários:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(135, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(290, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Criar linhas para registro";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(32, 51);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(458, 205);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(32, 262);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(217, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Criar arquivo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(330, 262);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(160, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Reiniciar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "*.txt";
+            this.saveFileDialog1.Filter = "Arquivos textos|*.txt";
+            this.saveFileDialog1.Title = "Dados para geração de arquivo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 297);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ GeracaoArquivoTexto/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeracaoArquivoTexto
+{
+    public partial class Form1 : Form
+    {
+
+        DataTable dt = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+            this.dt.Columns.Add("Nome", typeof(string));
+            this.dt.Columns.Add("Salário", typeof(double));
+            dataGridView1.DataSource = this.dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numeroFuncionarios = Convert.ToInt16(textBox1.Text);
+            if (numeroFuncionarios < 1) numeroFuncionarios = 1;
+            int i = 0;
+
+            do
+            {
+                //DataRow novaLinha = this.dt.NewRow();
+                this.dt.Rows.Add("", 0);
+
+                //var linhaTabela = new DataGridViewRow();
+                //linhaTabela.Cells.Add(new DataGridViewTextBoxCell {Value = string.Empty});
+                //linhaTabela.Cells.Add(new DataGridViewTextBoxCell { Value = 0 });
+                //this.dt.Rows.Add(linhaTabela);
+            } while (++i < numeroFuncionarios);
+            textBox1.Enabled = false;
+            button1.Enabled = false;
+            button3.Enabled = true;
+
+            dataGridView1.DataSource = this.dt;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Criar um novo objeto SaveFileDialog
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            // Definir as propriedades do SaveFileDialog
+            saveFileDialog.Filter = "Arquivos de Texto (*.txt)|*.txt|Todos os arquivos (*.*)|*.*";
+            saveFileDialog.Title = "Salvar Arquivo";
+            saveFileDialog.FileName = "exemplo.txt";
+            saveFileDialog.DefaultExt = "txt";
+            saveFileDialog.AddExtension = true;
+
+            // Exibir o SaveFileDialog e aguardar que o usuário faça uma seleção
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Obter o caminho e o nome do arquivo selecionado pelo usuário
+                string nomeArquivo = saveFileDialog.FileName;
+
+                // Escrever algum conteúdo no arquivo
+                using (StreamWriter sw = new StreamWriter(nomeArquivo))
+                {
+                    sw.WriteLine("Exemplo de conteúdo salvo em um arquivo usando SaveFileDialog");
+                }
+
+                // Exibir uma mensagem informando que o arquivo foi salvo com sucesso
+                MessageBox.Show("Arquivo salvo com sucesso em " + nomeArquivo, "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            //foreach (DataRow linha in dt.Rows)
+            //{
+             //   Console.WriteLine(linha[0]);
+              ///  Double valorAnterior = Double.Parse((String)linha[1], CultureInfo.InvariantCulture);
+
+               // Console.WriteLine(valorAnterior);
+           // }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("clicou aqui");
+            this.dt.Rows.Clear();
+            textBox1.Text = string.Empty;
+            textBox1.Enabled = true;
+            button1.Enabled = true;
+            button3.Enabled = false;
+        }
+    }
+}
